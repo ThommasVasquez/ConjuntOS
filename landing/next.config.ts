a3b01ea682b86cec2dac776e4ebbcad47e32fe0a
paulo <pauloxti@gmail.com>
2026-07-13 20:44:13 +0200

    separado landing

--- landing/next.config.ts
+++ landing/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   transpilePackages: ["gsap"],
   images: {
     unoptimized: true,
--- landing/out/_next/static/chunks/pages/_app-95dbebdbd42aa51f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{4605:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(5299)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(4605),n(9625))),_N_E=_.O()}]);
